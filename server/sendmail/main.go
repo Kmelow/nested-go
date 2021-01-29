b37d6399679c562a1dcc653f0e6529ea2a66c9ff
Camilo Coelho <camilo.c@protonmail.com>
2021-01-29 17:24:09 +0100

    Testing Go CI Tools

--- server/sendmail/main.go
+++ server/sendmail/main.go
@@ -1,34 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
-	"os"
 
-	"github.com/gin-gonic/gin"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 func main() {
-	router := gin.Default()
-	sm := router.Group("/api/v1")
-
-	sm.GET("/send-mail/ping", func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", "*")
-		c.Header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS")
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome"))
 	})
-	if err := router.Run(httpPort()); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func httpPort() string {
-	port := "6001"
-	if p := os.Getenv("PORT"); p != "" {
-		port = p
-	}
-	return fmt.Sprintf(":%s", port)
+	http.ListenAndServe(":3000", r)
 }
