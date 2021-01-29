be317143cab45907c0f5bcc00740c486ce3c5348
Camilo Coelho <camilo.c@protonmail.com>
2021-01-29 17:20:55 +0100

    Testing Go CI Tools

--- server/sendmail/main.go
+++ server/sendmail/main.go
@@ -2,8 +2,33 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Changifdsng fds the sendmail service")
+	router := gin.Default()
+	sm := router.Group("/api/v1")
+
+	sm.GET("/send-mail/ping", func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS")
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	if err := router.Run(httpPort()); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func httpPort() string {
+	port := "6001"
+	if p := os.Getenv("PORT"); p != "" {
+		port = p
+	}
+	return fmt.Sprintf(":%s", port)
 }
