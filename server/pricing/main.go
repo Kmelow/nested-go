39f1df496ecb5dc9c314277f20367797e5a3abf8
Camilo Coelho <camilo.c@protonmail.com>
2021-01-29 13:50:46 +0100

    Testing dorny - shell y

--- server/pricing/main.go
+++ server/pricing/main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	fmt.Println("Creating agafdin again and again the pricing service")
+	fmt.Println("Creating agfdsafdin again and again the pricing service")
 }
--- server/sendmail/packGreet/logic.go
+++ server/sendmail/packGreet/logic.go
@@ -4,5 +4,5 @@ import "fmt"
 
 // Greet returns greeting
 func Greet(s string) string {
-	return fmt.Sprintf("Au fdrevoir.... %s. Now in Shell", s)
+	return fmt.Sprintf("Au fdfdsrevoir.... %s. Now in Shell", s)
 }
